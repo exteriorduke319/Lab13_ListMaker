7fd59b509c712f87959c88b1ae64915e505bc821
exteriorduke319 <yarnevictanner@gmail.com>
2024-03-21 23:13:27 -0400

    47 minutes before it's due

--- src/IOHelper.java
+++ src/IOHelper.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class IOHelper {
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class IOHelper {
+    private static JFileChooser chooser = new JFileChooser();
+
+    public static String openFile(ArrayList<String> list) throws IOException {
+        try {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String rec = "";
+                System.out.println("File Path: " + file);
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    list.add(rec);
+                }
+                reader.close();
+                System.out.println("\n\nData file read!");
+                return file.getFileName().toString();
+            } else {
+                System.out.println("File not selected. Please restart program.");
+                System.exit(0);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+
+    public static void writeFile (ArrayList<String> list, String name) throws IOException {
+        ArrayList <String> recs = list;
+        String fileName = name;
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\" + name + ".txt");
+
+        try {
+            OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+
+            for (String r : recs) {
+                writer.write(r, 0, r.length());
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("Data file written!");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,75 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import javax.swing.*;
+import javax.swing.plaf.FileChooserUI;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public class Main {
     public static Scanner drake = new Scanner(System.in);
     public static ArrayList <String> strings = new ArrayList<>();
+    private static boolean saved = false;
+    private static boolean loaded = false;
+    private static boolean edited = false;
+    private static String fileName = null;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         boolean programRun = true;
+        boolean saveCurrent = false;
         String menuInput;
-        strings.addAll(Arrays.asList("Oven", "iPad", "SuperBowlXLIII", "Roethlisberger", "Tomlin", "Cardinals", "Steelers", "2009", "Hines", "Santonio"));
+
 
 
         while(programRun) {
-            menuInput = InputHelper.getRegExString(drake, "Options: \nA - Add an item to the list\nD - Delete an item from the list\nP - Print the list\nQ- Quit the program", "[AaDdPpQq]");
+            menuInput = InputHelper.getRegExString(drake, "Options: \nA - Add an item to the list\nD - Delete an item from the list\nV - View the list\nO - Open a file from disk\nS - Save the current file to disk\nC - Clear the current list\nQ- Quit the program", "[AaDdVvOoSsCcQq]");
 
             if (menuInput.equalsIgnoreCase("a")) {
                 add();
+                if (loaded) {
+                    saved = false;
+                    edited = true;
+                }
             } else if (menuInput.equalsIgnoreCase("d")) {
                 delete();
-            } else if (menuInput.equalsIgnoreCase("p")) {
-                print();
-                System.out.println();
+                if (loaded) {
+                    saved = false;
+                    edited = true;
+                }
+            } else if (menuInput.equalsIgnoreCase("v")) {
+                view();
+            } else if (menuInput.equalsIgnoreCase("o")) {
+                if (edited) {
+                    if (loaded) {
+                        IOHelper.writeFile(strings, fileName);
+                    }
+                }
+                else {
+                    saveCurrent = InputHelper.getYNConfirm(drake, "Do you want to save the current list before opening a new one?");
+                    if (saveCurrent) {
+                        IOHelper.writeFile(strings, InputHelper.getNonZeroLenString(drake, "What would you like to name the file?"));
+                    }
+                }
+
+                clear();
+                fileName = IOHelper.openFile(strings);
+                loaded = true;
+                edited = false;
+                saved = false;
+            } else if (menuInput.equalsIgnoreCase("s")) {
+                save();
+            } else if (menuInput.equalsIgnoreCase("c")) {
+                clear();
             } else {
                 if (quit()) {
+                    boolean saveCheck = InputHelper.getYNConfirm(drake, "Would you like to save? Y or N");
+                    if (saveCheck) {
+                        save();
+                    }
                     System.out.println("Thank you shawty!");
                     programRun = false;
                 }
@@ -38,19 +84,44 @@ public class Main {
     }
 
     public static void delete() {
-        print();
+        for (int i = 0; i < strings.size(); i++) {
+            System.out.print(i + " - " + strings.get(i) + ", ");
+        }
+        System.out.println();
+
         strings.remove(InputHelper.getRangedInt(drake, "What index would you like to delete?", 0, strings.size()));
         System.out.println();
     }
 
-    public static void print() {
+    public static void save() throws IOException {
+        if (loaded) {
+            IOHelper.writeFile(strings, fileName);
+        } else {
+            IOHelper.writeFile(strings, InputHelper.getNonZeroLenString(drake, "File Name: "));
+        }
+
+        saved = true;
+        edited = false;
+
+        if (!InputHelper.getYNConfirm(drake, "Would you like to keep working on this file?")) {
+            clear();
+            loaded = false;
+        }
+    }
+    public static void view() {
         for (int i = 0; i < strings.size(); i++) {
-            System.out.print(i + " - " + strings.get(i) + ", ");
+            System.out.println(i + " - " + strings.get(i));
         }
         System.out.println();
     }
 
     public static boolean quit() {
         return InputHelper.getYNConfirm(drake, "Are you sure you want to quit? [Y/N]");
+    }
+
+    public static void clear() {
+        for (int i = 0; i < strings.size(); i++) {
+            strings.remove(i);
+        }
     }
 }
